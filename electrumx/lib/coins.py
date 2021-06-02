61e4ae26a3b06bffccb3f96be9c904b48cf1198a
Pungo Token <45178410+pungotoken@users.noreply.github.com>
2021-06-02 21:51:32 +0200

    Update coins.py

--- electrumx/lib/coins.py
+++ electrumx/lib/coins.py
@@ -1121,6 +1121,33 @@ class Bitcoinzero(Coin):
     REORG_LIMIT = 5000
     PEERS = []
 
+class GoByte(Coin):
+    NAME = "GoByte"
+    SHORTNAME = "GBX"
+    NET = "mainnet"
+    XPUB_VERBYTES = bytes.fromhex("0488B21E")
+    XPRV_VERBYTES = bytes.fromhex("0488ADE4")
+    GENESIS_HASH = ('0000033b01055cf8df90b01a14734cae'
+                    '92f7039b9b0e48887b4e33a469d7bc07')
+    P2PKH_VERBYTE = bytes.fromhex("26")
+    P2SH_VERBYTES = [bytes.fromhex("0A")]
+    WIF_BYTE = bytes.fromhex("C6")
+    TX_COUNT_HEIGHT = 115890
+    TX_COUNT = 245030
+    TX_PER_BLOCK = 4
+    RPC_PORT = 12454
+    PEERS = [
+        'electrum1-gbx.polispay.org',
+        'electrum2-gbx.polispay.org'
+    ]
+    SESSIONCLS = DashElectrumX
+    DAEMON = daemon.DashDaemon
+
+    @classmethod
+    def header_hash(cls, header):
+        '''Given a header return the hash.'''
+        import neoscrypt
+        return neoscrypt.getPoWHash(header)
 
 class Unitus(Coin):
     NAME = "Unitus"
